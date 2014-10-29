bcd692b9215011109b90d260519b17bbb1d70ff0
Fritjof Büttner <mail@fritjofbuettner.com>
2014-10-29 11:51:20 +0100

    toroidal field properties

--- field.py
+++ field.py
@@ -1,1 +1,25 @@
 __author__ = 'fbuettner'
+
+class Field:
+    """
+    the playground
+    """
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_new_coordinates(self, current_x, current_y, delta_x, delta_y):
+        new_x = current_x + delta_x
+        if new_x <= 0:
+            new_x += self.width
+        elif new_x > self.width:
+            new_x -= self.width
+
+        new_y = current_y + delta_y
+        if new_y <= 0:
+            new_y += self.height
+        elif new_y > self.height:
+            new_y -= self.height
+
+        return new_x, new_y
+
