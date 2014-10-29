424ef476a93473e964e79b0f8aa6b477fbd4aca2
Fritjof Büttner <mail@fritjofbuettner.com>
2014-10-29 15:17:33 +0100

    policy get directions test

--- unittests/policy_test.py
+++ unittests/policy_test.py
@@ -1,9 +1,19 @@
 from policy import Policy
 import unittest
+from collections import Counter
 
-class testField(unittest.TestCase):
+class testPolicy(unittest.TestCase):
     def setUp(self):
         self.pol = Policy(0.2, 0.2, 0.2, 0.2, 0.2)
 
 
+    def test_get_direction(self):
+        directions = []
+        for i in xrange(100):
+            directions.append(self.pol.get_direction())
+        c = Counter(directions)
+        print c.values()
+        self.assertEqual(len(c.values()), 5)
 
+if __name__ == '__main__':
+    unittest.main()
