2401d3d877f309a136318124c0a1dbda2d19c438
Fritjof Büttner <mail@fritjofbuettner.com>
2014-10-29 15:37:29 +0100

    update probability sum warning to general inequality

--- policy.py
+++ policy.py
@@ -10,8 +10,8 @@ class Policy:
 
     def __init__(self, prob_stay, prob_north, prob_east, prob_south, prob_west):
         prob_sum = prob_stay + prob_north + prob_east + prob_south + prob_west
-        if prob_sum > 1.0:
-            warning("Sum of probabilities larger than 1, will be normalized to 1.")
+        if prob_sum != 1.0:
+            warning("Sum of probabilities different from 1, will be normalized to 1.")
         self.prob_stay = prob_stay / prob_sum
         self.prob_north = prob_north / prob_sum
         self.prob_east = prob_east / prob_sum
