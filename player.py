49a2b1e5caaa69b9213dc4a5b82fc6efbb3a87f3
Bas Veeling <basveeling@gmail.com>
2014-10-29 13:45:34 +0100

    Added move function to player

--- player.py
+++ player.py
@@ -6,6 +6,11 @@ class Player:
     superclass for predator and prey
     implements common properties like position
     """
-    def __init__(self, x, y):
+    def __init__(self, x, y, field):
         self.x = x
         self.y = y
+        self.field = field
+
+    def move(self,delta_x, delta_y):
+        assert (abs(delta_x) + abs(delta_y) = 1), 'Only non-diagonal steps of 1 are allowed'
+        self.x, self.y = self.field.get_new_coordinates(self.x, self.y, delta_x, delta_y)
