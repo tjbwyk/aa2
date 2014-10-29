f3516459b6717bf2cbcc567b599980cddb38bc9e
Fritjof Büttner <mail@fritjofbuettner.com>
2014-10-29 16:51:17 +0100

    fix some bugs

--- preypolicy.py
+++ preypolicy.py
@@ -1,35 +1,34 @@
-
 class PreyPolicy(Policy):
-  """
+    """
     implementation of the policy of the prey
   """
-	
-  fixedActions = [(0.8, (0, 0))]
-  flexActions = [(0, -1), (-1, 0), (0, 1), (1, 0)]
+    def __init__(self):
+        self.fixedActions = [(0.8, (0, 0))]
+        self.flexActions = [(0, -1), (-1, 0), (0, 1), (1, 0)]
 
-  def updatePolicy(field, agent):
-    """
-      updates the probabilities of the states
-    """
-     
-   newStates = []
+    def updatePolicy(self, field, agent):
+        """
+        updates the prey policy
+        :param field:
+        :param agent: owner of the policy
+        :return:
+        """
+        newStates = []
 
-    probability = 1
-    for fixed in fixedActions:
-      prob, action = fixed
-      probability -= prob
+        probability = 1
+        for fixed in self.fixedActions:
+            prob, action = fixed
+            probability -= prob
 
-	
-    for flex in flexActions:
-      newStates.append(field.get_new_coordinates(agent.position, flex))  	
+        for flex in self.flexActions:
+            newStates.append(field.get_new_coordinates(agent.location, flex))
 
-    for agent in field.getPredators():
-      if agent.__class__.__name__==Predator.__name__:
-        newStates.remove(agent.location)
+        for agent in field.getPredators():
+            newStates.remove(agent.location)
 
-    flexProb = probabilty / len(newStates)  
+        flexProb = probability / len(newStates)
 
-    allStates = [ (flexProb, x) for x in newStates ]
-    allStates.extend(fixedActions)
+        allStates = [(flexProb, x) for x in newStates]
+        allStates.extend(self.fixedActions)
 
-    return allStates
+        return allStates
