bee9015c5cd96fa6e5a7e7f6d0bfe4c652c4a3cc
Fritjof Büttner <mail@fritjofbuettner.com>
2014-11-12 22:14:53 +0100

    create environment fo unittest #11

--- unittests/get_probability_test.py
+++ unittests/get_probability_test.py
@@ -1,9 +1,15 @@
 __author__ = 'fbuettner'
 import unittest
+from field import Field
+from predator import Predator
+from predatorpolicy import PredatorPolicy
 
 class testField(unittest.TestCase):
     def setUp(self):
-        pass
+        self.environment = Field(11, 11)
+        self.fatcat = Predator((0, 0))
+        self.fatcat.policy = PredatorPolicy(self.fatcat, self.environment)
 
     def test_probability(self):
-        pass
+        probabilities, next_states = self.fatcat.policy.get_next_states((0,0))
+        print probabilities
