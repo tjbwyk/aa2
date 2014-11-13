ae2221216d9a2d4e4f92987fa23c980ca852ddae
Sebastian Droeppelmann <sebastian.droeppelmann@gmail.com>
2014-11-14 00:23:45 +0100

    added policy iteration, moved v into policy, added greedy selection of action

--- models/policies/policy.py
+++ models/policies/policy.py
@@ -8,6 +8,7 @@ class Policy:
     def __init__(self, agent, field, seed=None):
         self.agent = agent
         self.field = field
+        self.value = {state: 0.0 for state in field.get_all_states_with_terminal()}
 
         #initialize random number generator
         if seed is not None:
@@ -25,11 +26,34 @@ class Policy:
             move = random.random()
             # select the action that belongs to random move value
             probability = 0.0
-            actions = self.agent.get_actions()
             prob_map = self.get_probability_mapping(state)
             while move > probability and len(prob_map) > 0:
                 prob, action = prob_map.pop()
                 probability += prob
+            return action
+        elif style == "greedy":
+            val = 0
+            selected_states = []
+            for next_state in self.field.get_next_states(state):
+                if val < self.value[next_state]:
+                    selected_states = [next_state]
+                    val = self.value[next_state]
+                elif val == self.value[next_state]:
+                    selected_states.append(next_state)
+
+            if len(selected_states) == 1:
+                next_state = selected_states[0]
+            elif len(selected_states) > 1:
+                next_state = random.choice(selected_states)
+            else:
+                raise ValueError("no state found")
+            pred_pos, prey_pos = state
+            next_pred_pos, next_prey_pos = next_state
+            action = self.field.get_relative_position(pred_pos, next_pred_pos)
+
+            if action not in self.agent.get_actions():
+                raise ValueError("action not in legal actions of agent")
+
             return action
         else:
             # given style not recognized
---
+++ policy_iteration.py
@@ -0,0 +1,94 @@
+from models.field import Field
+from models.predator import Predator
+from models.prey import Prey
+from models.policies.random_predator_policy import RandomPredatorPolicy
+from models.policies.random_prey_policy import RandomPreyPolicy
+
+def main():
+    as013(verbose=False)
+
+def calculate_value(state, field, policy, value, discount_factor):
+    new_value = 0
+    for prob, action in policy.get_probability_mapping(state):
+        tmp_v = 0
+        for next_state in field.get_next_states(state):
+        #for next_state in all_states:
+            tmp_prob = policy.get_probability(state, next_state, action)
+            tmp_rew = field.get_reward(next_state) + discount_factor * value[next_state]
+            tmp_v += tmp_prob * tmp_rew
+        new_value += prob * tmp_v
+    return new_value
+
+def iterative_policy_evaluation(field, discount_factor, all_states, change_epsilon = 0.001):
+    # Initialise value array and temp array:
+    policy = field.get_predator().policy
+    value = policy.value
+
+    iterations = 0
+    goOn = True
+    while goOn:
+        delta_value = 0
+        iterations += 1
+        for state in all_states:
+            temp_value = value[state]
+            value[state] = calculate_value(state, field, policy, value, discount_factor)
+            delta_value = max(delta_value, abs(temp_value - value[state]))
+        if delta_value < change_epsilon:
+            goOn = False
+
+    return value
+
+
+def init_environment():
+    field = Field(11, 11)
+    predator = Predator((0, 0))
+    predator.policy = RandomPredatorPolicy(predator, field)
+    chip = Prey((5, 5))
+    chip.policy = RandomPreyPolicy(chip, field)
+    field.add_player(predator)
+    field.add_player(chip)
+    return field
+
+
+
+def calculate_argmax(state, field, policy, value, discount_factor):
+
+    action = policy.pick_next_action(state, style="greedy")
+    tmp_v = 0
+    for next_state in field.get_next_states(state):
+        tmp_prob = policy.get_probability(state, next_state, action)
+        tmp_rew = field.get_reward(next_state) + discount_factor * value[next_state]
+        tmp_v += tmp_prob * tmp_rew
+    return tmp_v
+
+
+def policy_improvement(field, value, discount_factor, all_states):
+    policy = field.get_predator().policy
+    policy_stable = True
+    for state in all_states:
+        b = policy.value[state]
+        policy.value[state] = calculate_argmax(state, field, policy, value, discount_factor)
+        if b != policy.value[state]:
+            policy_stable = False
+
+    return policy_stable
+
+def as013(verbose=True):
+    field = init_environment()
+    #calc once, since all states are always the same
+    all_states = field.get_all_states()
+    discount_factor = 0.9
+    iterations = 0
+    end_loop = False
+
+
+    while not end_loop:
+        iterations += 1
+        value = iterative_policy_evaluation(field, discount_factor, all_states)
+        end_loop = policy_improvement(field, value, discount_factor, all_states)
+
+    print iterations
+
+
+if __name__ == '__main__':
+    main()
