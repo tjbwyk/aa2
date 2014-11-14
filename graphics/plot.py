178bcbaf4abcdb0c46acddac0477a66611162dcf
Fritjof Büttner <mail@fritjofbuettner.com>
2014-11-14 16:51:44 +0100

    avoid multiple colorbars, replot

--- graphics/plot.py
+++ graphics/plot.py
@@ -9,6 +9,7 @@ def value_heatmap(values, colorbar=True, path=None):
     :param path: if given, save the plot to pdf at given path.
     :return:
     """
+    plt.figure()
     plt.xlim(0, values.shape[1])
     plt.ylim(0, values.shape[0])
     heatmap = plt.pcolor(values)
