cd19061153e9e779b33ed47038ce5a9639b28b45
Fritjof Büttner <mail@fritjofbuettner.com>
2014-11-14 17:22:06 +0100

    extracted random policy assignment to extra script. now the main file really only calls the tasks

--- main.py
+++ main.py
@@ -1,56 +1,15 @@
-import timeit
-import time
-import numpy as np
-from models.field import Field
-from models.predator import Predator
-from models.prey import Prey
-from models.policies.random_predator_policy import RandomPredatorPolicy
-from models.policies.random_prey_policy import RandomPreyPolicy
-
-def main(n_runs=1):
-    # start the first assignment
-    # run 100 times and measure time
-    runtimes = []
-    iterations = []
-    for n in xrange(n_runs):
-        start = timeit.default_timer()
-        i = as011(verbose=False)
-        time = timeit.default_timer() - start
-        runtimes.append(time)
-        iterations.append(i)
-    runtimes = np.asarray(runtimes)
-    iterations = np.asarray(iterations)
-    print "Mean runtime: " + str(np.mean(runtimes)) + " (standard deviation: " + str(np.std(runtimes)) + ")"
-    print "Mean number of iterations: " + str(np.mean(iterations)) + " (standard deviation: " + str(np.std(iterations)) + ")"
-    return runtimes, iterations
+from random_policy import random_policy_wrapper
+from iterative_policy_evaluation import as012
+from value_iteration import as014
 
 
-def as011(verbose=True):
-    # create field and characters
-    environment = Field(11, 11)
-    fatcat = Predator((0, 0))
-    fatcat.policy = RandomPredatorPolicy(fatcat, environment)
-    chip = Prey((5, 5))
-    chip.policy = RandomPreyPolicy(chip, environment)
-    environment.add_player(fatcat)
-    environment.add_player(chip)
-
-    if verbose:
-        print environment.print_field()
-        time.sleep(1)
-
-    i = 0
-    while not environment.is_ended():
-        fatcat.act()
-        chip.act()
-        print environment
-        i += 1
-
-    if verbose:
-        print str(i) + " iterations"
-        print environment.print_field()
-    return i
+def main():
+    random_policy_wrapper(n_runs=100)
+    # iterative policy evaluation
+    as012()
+    # value iteration
+    as014(plot_values=True)
 
 
 if __name__ == '__main__':
-    main(n_runs=100)
+    main()
