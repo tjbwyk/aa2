cd60792347ca5ab39baad0c3994783ea2b27c16a
Bas Veeling <basveeling@gmail.com>
2014-11-24 12:40:07 +0100

    Qlearning plot script that can plot graphs for all possible combinations of parameters

--- q_learning.py
+++ q_learning.py
@@ -1,11 +1,10 @@
-import numpy as np
+
 from models.field import Field
 from models.predator import Predator
 from models.prey import Prey
 from models.policies.predatorpolicy import PredatorPolicy
 from models.policies.random_prey_policy import RandomPreyPolicy
 import collections
-import matplotlib.pyplot as plt
 
 def max_action_value_for_q(Q_value, state, predator):
     max = 0
@@ -16,7 +15,7 @@ def max_action_value_for_q(Q_value, state, predator):
 
     return max
 
-def compute_q_value(Q_value, cur_state, action, reward, new_state, predator, learning_rate = 0.2, discount_factor = 0.5):
+def compute_q_value(Q_value, cur_state, action, reward, new_state, predator, learning_rate = 0.1, discount_factor = 0.9):
     result = Q_value[cur_state,action] +\
         learning_rate * (
             reward +
@@ -25,9 +24,7 @@ def compute_q_value(Q_value, cur_state, action, reward, new_state, predator, lea
         )
     return result
 
-def run_q_learning(verbose=True):
-    NUM_EPISODES = 15000
-
+def run_q_learning(learning_rate = 0.1, discount_factor = 0.9, epsilon=0.1, value_init= 15, num_episodes=1000, verbose=True):
     # Initialize env:
     field = Field(11, 11)
     predator = Predator((0, 0))
@@ -37,40 +34,36 @@ def run_q_learning(verbose=True):
     field.add_player(predator)
     field.add_player(chip)
 
-    # Initialize Q(s,a) optimistically with a value of 15
-    q_value = collections.defaultdict(lambda: 15)
+    # Initialize Q(s,a) optimistically with a value of value_init
+    q_value = collections.defaultdict(lambda: value_init)
 
     # Set policy
     policy = PredatorPolicy(predator,field,qvalue=q_value)
 
     episode_runs = []
-    for i in range(1,NUM_EPISODES):
+    for i in range(1,num_episodes):
         predator.location = (0,0)
         chip.location     = (5,5)
         cur_state = field.get_current_state()
         steps = 0
         while not field.is_ended():
-            action = policy.pick_next_action(cur_state, style="q-egreedy",epsilon=0.1)
+            action = policy.pick_next_action(cur_state, style="q-egreedy",epsilon=epsilon)
             field.take_action(action)
             reward = field.get_reward()
             new_state = field.get_current_state()
 
-            q_value[cur_state,action] = compute_q_value(q_value, cur_state, action, reward, new_state, predator)
+            q_value[cur_state,action] = compute_q_value(q_value, cur_state, action, reward, new_state, predator, learning_rate, discount_factor)
 
             cur_state = new_state
             steps += 1
         episode_runs.append(steps)
-        if i % (NUM_EPISODES/10) == 0:
-            print i
 
+        if verbose:
+            if i % (num_episodes/10) == 0:
+                print i,
+    print
+    return episode_runs
 
-    # for key in q_value:
-    #     if q_value[key] > 1:
-    #         print key
-    #         print q_value[key]
-    plt.plot(episode_runs)
-    plt.show()
-    print len(q_value)
 if __name__ == '__main__':
     run_q_learning(verbose=True)
     print "Done."
