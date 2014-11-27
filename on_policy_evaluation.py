e539388496fd61576025f0b3013a469f9f463444
Fritjof Büttner <mail@fritjofbuettner.com>
2014-11-27 14:19:47 +0100

    reformat on-policy MC

--- on_policy_evaluation.py
+++ on_policy_evaluation.py
@@ -1,25 +1,16 @@
 __author__ = 'sebastian_droeppelmann'
-import timeit
-
+"""
+On Policy Monte-Carlo Control
+"""
 import numpy as np
-import pandas
-
 import models.field
-from models.predator import Predator
-from models.prey import Prey
-from models.policies.random_predator_policy import RandomPredatorPolicy
-from models.policies.random_prey_policy import RandomPreyPolicy
-from graphics import plot
 
-import random
 
-def on_policy_montecarlo(discount_factor = 0.7):
-
+def on_policy_montecarlo(discount_factor=0.7):
     returns_list = []
 
     field = models.field.init_default_environment()
     field.pick_random_start()
-
 
     while not field.is_ended():
         pred_act, prey_act, reward = field.act()
@@ -29,20 +20,22 @@ def on_policy_montecarlo(discount_factor = 0.7):
     first_visit(returns_list, discount_factor, field.get_predator().policy, reward)
     fill_policy(returns_list, field.get_predator().policy, reward)
 
+
 def fill_policy(sa_list, policy, epsilon):
     states = set(s for s, a in sa_list)
     for state in states:
         q_list = policy.return_q_values(state)
         tmpact = None
         tmpy = -np.inf
-        for x,y in q_list:
+        for x, y in q_list:
             if y > tmpy:
                 tmpact = x
                 tmpy = y
 
-        update_prob_mapping = [(epsilon/len(q_list), act) for act in policy.agent.get_actions()]
-        update_prob_mapping.append(((1-epsilon+epsilon/len(q_list)), tmpact))
+        update_prob_mapping = [(epsilon / len(q_list), act) for act in policy.agent.get_actions()]
+        update_prob_mapping.append(((1 - epsilon + epsilon / len(q_list)), tmpact))
         policy.default_probmapping[state] = update_prob_mapping
+
 
 def first_visit(sa_orig_list, discount_factor, policy, reward):
     i = 0
@@ -57,6 +50,7 @@ def first_visit(sa_orig_list, discount_factor, policy, reward):
             returns_value.append(discount_factor ^ i * reward)
             state, action = sa
             policy.q_value[state, action] = np.average(returns_value)
+
 
 if __name__ == '__main__':
     on_policy_montecarlo()
