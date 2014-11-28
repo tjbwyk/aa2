bb918a1e424f0af4754c531dfa10ae4429ec818f
Fritjof Büttner <mail@fritjofbuettner.com>
2014-11-28 17:58:43 +0100

    only display last learned policy in GUI

--- sarsa.py
+++ sarsa.py
@@ -1,3 +1,5 @@
+import time
+
 from models.field import Field
 from models.predator import Predator
 from models.prey import Prey
@@ -5,42 +7,45 @@ from models.policies.random_predator_policy import RandomPredatorPolicy
 from models.policies.random_prey_policy import RandomPreyPolicy
 from graphics.gui import GameFrame
 from graphics.plot import action_value_quiver_relative
-import collections
-import time
 
-def calculate_q_value_percentage(field,q_value):
+
+def calculate_q_value_percentage(field, q_value):
     count = 0
     all_states = field.get_all_states()
     for state in all_states:
-        max_action,_ = max_action_for_q(q_value, state, field.get_predator())
+        max_action, _ = max_action_for_q(q_value, state, field.get_predator())
         best_actions = field.get_best_actions(state)
         if max_action in best_actions:
             count += 1
-    return float(count) /  float(len(all_states))
+    return float(count) / float(len(all_states))
+
 
 def max_action_for_q(Q_value, state, predator):
     max = 0
-    max_action = (0,0)
+    max_action = (0, 0)
     for action in predator.get_actions():
         tmp = Q_value[state, action]
         if tmp > max:
             max = tmp
             max_action = action
 
-    return max_action,max
+    return max_action, max
 
-def compute_q_value(Q_value, old_state, old_action, new_action, reward, new_state, predator, learning_rate=0.1, discount_factor=0.9):
 
+def compute_q_value(Q_value, old_state, old_action, new_action, reward, new_state, predator, learning_rate=0.1,
+                    discount_factor=0.9):
     result = Q_value[old_state, old_action] + \
              learning_rate * (
                  reward +
-                 discount_factor * Q_value[new_state,new_action] -
+                 discount_factor * Q_value[new_state, new_action] -
                  Q_value[old_state, old_action]
              )
     return result
 
-def run(learning_rate=0.1, discount_factor=0.9, epsilon=0.1, tau=0.1, value_init=15, num_episodes=1000, verbose=True,policy_style="q-egreedy",
-                   gui=False, plot=False):
+
+def run(learning_rate=0.1, discount_factor=0.9, epsilon=0.1, tau=0.1, value_init=15, num_episodes=1000, verbose=True,
+        policy_style="q-egreedy",
+        gui=False, plot=False):
     """
     Run multiple Q-Learning episodes
     :param learning_rate: the learning rate (alpha)
@@ -62,10 +67,6 @@ def run(learning_rate=0.1, discount_factor=0.9, epsilon=0.1, tau=0.1, value_init
     field.add_player(predator)
     field.add_player(chip)
 
-    # set GUI
-    if gui:
-        GUI = GameFrame(field=field)
-
     episode_runs = []
     percentage_correct = []
     for i in range(1, num_episodes):
@@ -74,38 +75,48 @@ def run(learning_rate=0.1, discount_factor=0.9, epsilon=0.1, tau=0.1, value_init
         chip.location = (5, 5)
         old_state = field.get_current_state()
         # Choose A from S using policy derived from Q (e.g., e-greedy)
-        old_action = predator.policy.pick_next_action(old_state,style=policy_style, epsilon=epsilon,tau=tau)
-        if gui:
-            GUI.draw_state(field.get_current_state_complete())
+        old_action = predator.policy.pick_next_action(old_state, style=policy_style, epsilon=epsilon, tau=tau)
         steps = 0
         while not field.is_ended():
             # Take action A, observe R, S'
             _, prey_action, reward = field.act_with_action(old_action)
             new_state = field.get_current_state()
             # Choose A' from S' using policy derived from Q (e.g., e-greedy)
-            new_action = predator.policy.pick_next_action(new_state,style=policy_style, epsilon=epsilon,tau=tau)
+            new_action = predator.policy.pick_next_action(new_state, style=policy_style, epsilon=epsilon, tau=tau)
 
             # Q(S, A) <- Q(S, A) + a[R + yQ(S', A') - Q(S, A)]
-            predator.policy.q_value[old_state, old_action] = compute_q_value(field.get_predator().policy.q_value, old_state, old_action, new_action, reward, new_state, predator, learning_rate, discount_factor)
+            predator.policy.q_value[old_state, old_action] = compute_q_value(field.get_predator().policy.q_value,
+                                                                             old_state, old_action, new_action, reward,
+                                                                             new_state, predator, learning_rate,
+                                                                             discount_factor)
 
             # S <- S'; A <- A';
             old_state = new_state
             old_action = new_action
-            if gui:
-                GUI.update()
-                time.sleep(0.02)
             steps += 1
         episode_runs.append(steps)
-        if (i-1) % ((num_episodes) / 300) == 0:
-            percentage_correct.append(calculate_q_value_percentage(field,predator.policy.q_value))
+        if (i - 1) % ((num_episodes) / 300) == 0:
+            percentage_correct.append(calculate_q_value_percentage(field, predator.policy.q_value))
         else:
             percentage_correct.append(percentage_correct[-1])
         if verbose:
             if i % (num_episodes / 10) == 0:
                 print i,
     print
+
+    if gui:
+        GUI = GameFrame(field=field)
+        time.sleep(1)
+        field.pick_random_start()
+        GUI.update(trace=False)
+        while not field.is_ended():
+            field.act(style=policy_style, epsilon=epsilon, tau=tau)
+            GUI.update()
+            time.sleep(0.2)
+        raw_input("Press Enter to terminate.")
+
     if plot:
-        #action_values_relative(q_value, relative_state=(-3, 3), path="qlearning_values.pdf")
+        # action_values_relative(q_value, relative_state=(-3, 3), path="qlearning_values.pdf")
         title = "Relative actions after " + str(num_episodes) + " episodes of Q-Learning"
         action_value_quiver_relative(predator.policy.q_value, path="sarsa_arrows.pdf", title=title)
     return episode_runs, predator.policy.q_value, percentage_correct
