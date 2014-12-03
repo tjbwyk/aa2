487e8a0abc2109160961e9ec6efe435154efe177
Sebastian Droeppelmann <sebastian.droeppelmann@gmail.com>
2014-12-03 16:24:31 +0100

    probabilistic policy now working again

--- models/policies/probabilistic_policy.py
+++ models/policies/probabilistic_policy.py
@@ -10,15 +10,19 @@ class ProbabilisticPolicy(Policy):
     def __init__(self, agent, field, value_init=None):
         super(Policy, self).__init__(agent, field, value_init)
 
-    def pick_next_action(self, state):
-            # move = random.random()
-            # # select the action that belongs to random move value
-            # probability = 0.0
-            # prob_map = self.get_probability_mapping(state)
-            # while move > probability and len(prob_map) > 0:
-            #     prob, action = prob_map.pop()
-            #     probability += prob
-            # if 'action' not in locals():
-            #     print state
-            # return action
-        pass
+    def pick_next_action(self, _state):
+        move = random.random()
+        # select the action that belongs to random move value
+        probability = 0.0
+        prob_map = self.get_probability_mapping()
+        while move > probability and len(prob_map) > 0:
+            prob, action = prob_map.pop()
+            probability += prob
+        return action
+
+    def get_probability_mapping(self):
+        """
+        returns the probability mapping for the possible actions of the agent
+        :return: a list of possible actions with the probability
+        """
+        return [(1/len(self.agent.get_actions()), action) for action in self.agent.get_actions()]
