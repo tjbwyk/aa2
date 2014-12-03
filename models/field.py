2ff6d0b8cbb2d19531b117688487cfc8e443f7b9
Fritjof Büttner <mail@fritjofbuettner.com>
2014-12-03 15:12:34 +0100

    add run_step() and transition() functions to field, work in progress #29

--- models/field.py
+++ models/field.py
@@ -1,7 +1,10 @@
 import numpy as np
+import random
 
+from models.player import Player
 from models.predator import Predator
 from models.prey import Prey
+from models.state import State
 
 
 def init_default_environment(pred_loc=(0, 0), prey_loc=(5, 5), value_init=30):
@@ -15,21 +18,53 @@ class Field(object):
     Models the environment:
     Responsibilities:
     - Maintaining a list of agents
-    -
+    - coordination of the steps in an episode
     """
 
     def __init__(self, width, height):
         self.width = width
         self.height = height
         self.players = []
+        self.state = None
         self.steps = 0
 
     def __str__(self):
         result = map(lambda p: str(p) + "(" + str(p.location[0]) + "," + str(p.location[1]) + ")", self.players)
         return ", ".join(result)
 
-    def act(self, **kwargs):
-        raise NotImplementedError
+    def run_step(self):
+        """
+        Runs a step of the current episode by telling all agents to pick their respective next action based on the
+        state of the field. Then, it computes the locations of all players in the next state and possible rewards.
+        Finally, new state and reward is distributed to all players so they can learn from their action.
+        :return:
+        """
+        actions = []
+        # for every player:
+        for agent in self.players:
+            # call act() function on player and get desired action in return
+            actions
+        # compute next state based on actions chosen by players
+        # tell each player their new location
+        pass
+
+    def transition(self, player, action):
+        """
+        This function determines what state the desired action of an agent leads to. For example, if the agent has a
+        tripping_probability > 0, it may end up in the same state it came from.
+        :param player: the player who wants to take the action
+        :param action: the desired action
+        :return: the new state of the agent
+        """
+        # tripping?
+        if random.random() < player.tripping_prob:
+            new_state = player.location
+        else:
+            new_state = self.get_new_coordinates(player.location, action)
+
+
+    def is_ended(self):
+        return self.state.is_terminal()
 
     def get_new_coordinates(self, current_location, delta):
         (x, y) = current_location
@@ -64,15 +99,6 @@ class Field(object):
 
     def get_preys(self, exception_player=None):
         return self.get_players_of_class(Prey, exception_player)
-
-    def is_ended(self):
-        """
-        checks if all preys have been found by a predator
-        :return: true if game is over
-        """
-        # 1) Predators collide
-        # 2) Predator catches pray
-        raise NotImplementedError, "Not sure if this is the right way to go"
 
     def get_reward(self, state=None):
         """
