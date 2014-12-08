b9305f330567419fcbf04e569a9e347ece1167c4
Sebastian Droeppelmann <sebastian.droeppelmann@gmail.com>
2014-12-08 18:54:01 +0100

    added merged greedy policy with documentation.

--- models/policies/greedy_policy.py
+++ models/policies/greedy_policy.py
@@ -1,4 +1,6 @@
 from models.policies.policy import Policy
+import random
+import numpy as np
 
 
 class GreedyPolicy(Policy):
@@ -6,61 +8,78 @@ class GreedyPolicy(Policy):
     The greedy policy
     """
 
-    def __init__(self, agent, field, value_init=None, epsilon=.0, gamma=.0):
+    def __init__(self, agent, field, value_init=None, epsilon=.0, gamma=.0, q_value_select=False):
         super(Policy, self).__init__(agent, field, value_init)
         self.epsilon = epsilon
         self.gamma = gamma
+        self.q_value_select = q_value_select
 
     def pick_next_action(self, state):
-            # if random.random() <= self.epsilon:
-            #     return self.pick_next_action(state, style="probabilistic")
-            # else:
-            #     val = 0
-            #     selected_states = []
-            #     for next_state, action in self.field.get_next_states_relative(state, with_actions=True):
-            #         if val < self.value[next_state]:
-            #             selected_states = [(next_state, action)]
-            #             val = self.value[next_state]
-            #         elif val == self.value[next_state]:
-            #             selected_states.append((next_state, action))
-            #     if len(selected_states) == 1:
-            #         next_state, action = selected_states[0]
-            #     elif len(selected_states) > 1:
-            #         next_state, action = random.choice(selected_states)
-            #     else:
-            #         raise ValueError("no state found")
-            #     if action not in self.agent.get_actions():
-            #         raise ValueError("action not in legal actions of agent from State: ", state, ", NextState: ",
-            #                          next_state, ", action: ", action)
-            # return action
-            #             if "epsilon" in kwargs:
-            #     epsilon = kwargs.get("epsilon")
-            # else:
-            #     epsilon = 0
-            # if random.random() <= epsilon:
-            #     return self.pick_next_action(state, style="probabilistic")
-            # else:
-            #     max_qval = -1
-            #     max_action = (0, 0)
-            #     for next_action in self.agent.get_actions():
-            #         if max_qval < self.q_value[state, next_action]:
-            #             max_action = next_action
-            #             max_qval = self.q_value[state, next_action]
-            #     return max_action
-            # if "gamma" in kwargs:
-            #     gamma = kwargs.get("gamma")
-            # else:
-            #     raise ValueError("style max_value requires parameter epsilon.")
-            # action_value = 0
-            # for action in self.agent.get_actions():
-            #     tmp_v = 0
-            #     for next_state in self.field.get_next_states(state, action):
-            #         tmp_prob = self.get_probability(state, next_state, action)
-            #         tmp_rew = self.field.get_reward(next_state) + gamma * self.value[next_state]
-            #         tmp_v += tmp_prob * tmp_rew
-            #     if tmp_v > action_value:
-            #         best_action = action
-            #         action_value = tmp_v
-            # return best_action
+        """
+        picks the next action according to a greedy policy:
 
-        pass
+        In case self.q_value_select is set to True, it picks the highest value
+        according to the state and next_action combination.
+
+        In case of a gamma > 0 it picks the next action according to the reward and discounted value
+        of the next state
+
+        In case gamma = 0 and q_value_select = False, the highest value given the next state will be picked.
+
+        In case a epsilon of > 0 is given, a random action is chosen with epsilon chance.
+        otherwise one of the above happens
+
+        if there are more than 1 choices with equally high values one of them will be picked at random
+        :param state: the current state
+        :return: the best action for the current state
+        """
+
+        if random.random() <= self.epsilon:
+            return self.pick_random_action()
+        else:
+            max_val = -np.inf
+            selected_states = []
+
+            for next_action in self.agent.get_actions():
+                if self.q_value_select:
+                    if max_val < self.value[state, next_action]:
+                        selected_states = [(state, next_action)]
+                        max_val = self.value[state, next_action]
+                    elif max_val == self.value[state, next_action]:
+                        selected_states.append((next_state, next_action))
+
+                else:
+                    if self.gamma > 0:
+                        tmp_val = 0
+
+                    for next_state in self.field.get_next_states(state, next_action):
+                        if self.gamma > 0:
+                            tmp_prob = self.get_probability(state, next_state, next_action)
+                            tmp_rew = self.field.get_reward(next_state) + self.gamma * self.value[next_state]
+                            tmp_val += tmp_prob * tmp_rew
+                        else:
+                            if max_val < self.value[next_state]:
+                                selected_states = [(next_state, next_action)]
+                                max_val = self.value[next_state]
+                            elif max_val == self.value[next_state]:
+                                selected_states.append((next_state, next_action))
+
+                    if self.gamma > 0:
+                        if max_val < tmp_val:
+                            selected_states = [(next_state, next_action)]
+                            max_val = self.value[next_state]
+                        elif max_val == self.value[next_state]:
+                            selected_states.append((next_state, next_action))
+
+                if len(selected_states) == 1:
+                    best_state, best_action = selected_states[0]
+                elif len(selected_states) > 1:
+                    best_state, best_action = random.choice(selected_states)
+                else:
+                    raise ValueError("no state found")
+
+        if best_action not in self.agent.get_actions():
+            raise ValueError("action not in legal actions of agent from State: ", state, ", NextState: ",
+                         next_state, ", action: ", next_action)
+
+        return best_action
